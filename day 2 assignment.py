74954e991e31e9e52514c544d9c8653fd484d06d
Vikas1599 <98824343+Vikas1599@users.noreply.github.com>
2022-02-02 15:12:21 +0530

    day 2 assignment

---
+++ day 2 assignment.py
@@ -0,0 +1,6 @@
+y = input("lord of the rings,'type the alphabet'")
+
+if y=='l'or y=='r'or y=='o'or y=='t'or y=='e'or y=='i'or y=='g':
+    print('Congratulations you won the lottery')
+else:
+    print('Better luck next time')
